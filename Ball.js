c0762a1401f7272448b932ebbb28ba847f8a1508
Rachna Gupta <rachgupta200@gmail.com>
2021-12-01 15:07:22 -0500

    basic html with pool table and temporary ball class

---
+++ Ball.js
@@ -0,0 +1,7 @@
+function Ball(color, position) {
+    this.color = color;
+    this.position = position;
+    //find some vector2D function for position and velocity
+    this.velocity = 0;
+    this.moving = false;
+  }
---
+++ basic_board.js
@@ -0,0 +1,8 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("pool");
+base_image = new Image();
+base_image.src = 'pool.png';
+base_image.onload = function(){
+  ctx.drawImage(base_image,0,0);
+}
