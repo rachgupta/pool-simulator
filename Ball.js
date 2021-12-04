dcbbfc7e38b7acf1431be4e845ed753c9bcb8545
Rachna Gupta <rachgupta200@gmail.com>
2021-12-04 15:36:20 -0500

    added debugged ball poisiton functionality

--- Ball.js
+++ Ball.js
@@ -1,6 +1,19 @@
-function Ball(color, position) {
-    this.color = color;
-    this.position = position;
+function Ball(type, position) {
+  if(type=="8-ball")
+  {
+    this.imgsrc = "8-ball.png"
+  }
+  else if(type=="cue")
+  {
+    this.imgsrc = "cue.png"
+  }
+  else if(type=="ball")
+  {
+    this.imgsrc = "ball.png"
+  }
+    this.type = type;
+    this.positionX = 100;
+    this.positionY = 200;
     //find some vector2D function for position and velocity
     this.velocity = Vector2(0,0);
     this.moving = false;
@@ -10,11 +23,13 @@ function Ball(color, position) {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ball_img = new Image();
-    ball_img.src = "ball.png";
+    ball_img.src = this.imgsrc;
+    x = this.positionX;
+    y = this.positionY
     ball_img.onload = function(){
-        ctx.drawImage(ball_img, 100, 200);
+        ctx.drawImage(ball_img, x, y);
     }
 }
 
-let myball = new Ball();
+let myball = new Ball("8-ball", new Vector2(100,200));
 myball.draw();
